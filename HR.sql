5dc10e2fccab54ccbe7db207ca1c7d7cc33d857b
aaradharma5 <aaradharma@gmail.com>
2025-11-23 19:24:39 +0530

    Update comment in HR.sql file

--- HR.sql
+++ HR.sql
@@ -1,5 +1,5 @@
 -- ========================================
--- ORACLE HR SCHEMA (WITHOUT CONSTRAINTS)123
+-- ORACLE HR SCHEMA (WITHOUT CONSTRAINTS)12345
 -- ========================================
 
 -- 1️⃣ REGIONS
